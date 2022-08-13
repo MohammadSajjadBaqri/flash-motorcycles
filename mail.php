54eb66dc2ec53820462e290078c21d7d8228268a
Mohammad Sajjad Baqri <sajjadbaqri16@gmail.com>
2022-08-13 14:02:37 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php 
 (isset($_POST['submit'])){
-    $to = "whoeveriam09@gmail.com" // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
+    $to = "whoeveriam09@gmail.com";
+    $from = $_POST['email'];
     $name = $_POST['name'];
     $tel = $_POST['tel'];
     $date = $_POST['date'];
@@ -9,13 +9,13 @@
     $review = $_POST['problem'];
     $contactsource = $_POST['contact-source'];
     $signup = $_POST['sign-up'];
-    $subject = "Form submission (Contact Us- Flash Motos)";
-    $subject2 = "Copy of the Contact Us form submitted to Flash Motorcycles";
-    $message = "Name:" . $name . "\n\n" . "E-Mail:" . $from . "\n\n" . "Phone Number:" . $tel . "\n\n" . "Birth date:" . $date . "\n\n" . "Country:" . $country . "\n\n" ."Review:" . $review . "Agreement to newsletter:" . $signup . "\n\n" . "How to be contacted:". $contactsource . $_POST['message'];
-    $message2="Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+    $subject = ["Form submission (Contact Us- Flash Motos)"];
+    $subject2 = ["Copy of the Contact Us form submitted to Flash Motorcycles"];
+    $message = ["Name:" . $name . "\n\n" . "E-Mail:" . $from . "\n\n" . "Phone Number:" . $tel . "\n\n" . "Birth date:" . $date . "\n\n" . "Country:" . $country . "\n\n" ."Review:" . $review . "Agreement to newsletter:" . $signup . "\n\n" . "How to be contacted:". $contactsource . $_POST['message']];
+    $message2= ["Here is a copy of your message " . $name . "\n\n" . $_POST['message']];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to; }
+    $headers = ["From:" . $from];
+    $headers2 = ["From:" . $to;] }
 
      if (mail($to, $subject, $message, $headers)) {
         echo "Thank you. Your review has been sent to Flash Motorcycles. They will respond shortly.";
